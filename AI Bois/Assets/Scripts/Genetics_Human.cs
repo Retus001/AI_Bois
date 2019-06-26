f8cf08b245b874f1e159b7568e5e1975d3dd3b54
Retus001 <retus001@gmail.com>
2019-06-26 08:53:11 -0500

    Generations Update

---
+++ AI Bois/Assets/Scripts/Genetics_Human.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genetics_Human : MonoBehaviour
+{
+    public GameObject humanPrefab;
+
+    private bool advanceTime;
+
+    public void ToggleTime(bool _toggle)
+    {
+        advanceTime = _toggle;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ AI Bois/Assets/Scripts/Human.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Human : MonoBehaviour
+{
+    // Genes
+    /*
+     * Body Type
+     * Skin Color
+     * Eye Color
+     * 
+     * 
+     * 
+     */ 
+
+    public Image body;
+    public Image eyes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
