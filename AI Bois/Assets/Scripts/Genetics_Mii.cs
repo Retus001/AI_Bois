e0577218d876ff3e7dd774bf2163eb2bb79e0b77
Retus001 <retus001@gmail.com>
2019-06-27 14:57:41 -0500

    Mii Generations Final

--- AI Bois/Assets/Scripts/Genetics_Mii.cs
+++ AI Bois/Assets/Scripts/Genetics_Mii.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Genetics_Mii : MonoBehaviour
 {
+    public float timeDelay;
+
     public GameObject miiPrefab;
     [Range(0, 32)]
     public int miiAmmount;
     public GameObject miiWorld;
 
-    [Header("Test")]
-    public GameObject[] Fathers;
-    public GameObject[] Mothers;
+    public float bodyMutationAmmount;
+    [Range(0,1)]
+    public float colorMutationAmmount;
 
-    [Range(0, 1)]
-    public float mutationRate;
+    private GameObject[,] pairs;
 
     public Sprite[] heads;
     public Sprite[] eyes;
@@ -25,65 +26,120 @@ public class Genetics_Mii : MonoBehaviour
     public Color[] iriscolor;
 
     private List<GameObject> miis = new List<GameObject>();
-    private bool advanceTime;
+    private bool advanceTime = false;
+    private float currentTime;
 
-    public void ToggleTime(bool _toggle)
+    public void ToggleTime()
     {
-        advanceTime = _toggle;
+        advanceTime = !advanceTime;
     }
 
     public void PairMiis()
     {
         List<GameObject> singles =  new List<GameObject>();
-        GameObject[,] pairs = new GameObject[miis.Count/2,2];
+        pairs = new GameObject[miis.Count/2,2];
 
-        Debug.Log("miis Count: " + miis.Count);
+        int randomBreak = 99;
 
-        int randomBreak = 20;
-
-        Debug.Log("Adding Singles");
         for (int i = 0; i < miis.Count; i++)
         {
-            if (i >= miis.Count)
-                Debug.Log("OutOfRange");
             singles.Add(miis[i]);
         }
-        Debug.Log("Finished Singles");
 
-        Debug.Log("Setting Pairs");
-        Debug.Log("Singles Count: " + singles.Count);
-        Debug.Log("Pairs Length: " + pairs.Length);
-        for (int i = 0; i < pairs.Length; i++)
+        for (int i = 0; i < pairs.Length/2; i++)
         {
+            // Setting Random Father
             int randomFather = Random.Range(0, singles.Count);
+            pairs[i, 0] = singles[randomFather];
+            singles.RemoveAt(randomFather);
+
+            // Setting Random Mother
             int randomMother = Random.Range(0, singles.Count);
             while (randomMother == randomFather && randomBreak > 0)
             {
                 randomMother = Random.Range(0, singles.Count);
                 randomBreak--;
             }
-
             if (randomBreak <= 0)
                 Debug.Log("Infinite While");
-
-            Debug.Log("Setting Father " + i);
-            pairs[i, 0] = singles[randomFather];
-            Debug.Log("Setting Mother " + i);
             pairs[i, 1] = singles[randomMother];
+            singles.RemoveAt(randomMother);
 
-            singles.Remove(singles[randomFather]);
-            singles.Remove(singles[randomMother]);
+            // Set Pair Ids
+            pairs[i, 0].GetComponent<Human>().SetPairId(i);
+            pairs[i, 1].GetComponent<Human>().SetPairId(i);
         }
-        Debug.Log("Finished Pairs");
+        Debug.Log("Finished Setting Parents");
+    }
 
-        Fathers = new GameObject[pairs.Length];
-        Mothers = new GameObject[pairs.Length];
-
-        for (int i = 0; i < pairs.Length; i++)
+    public void NewGeneration()
+    {
+        for (int i = 0; i < miis.Count; i++)
         {
-            Fathers[i] = pairs[i, 0];
-            Mothers[i] = pairs[i, 1];
+            int whoFucked = Random.Range(0, pairs.Length / 2);
+            ProcreateMii(miis[i], pairs[whoFucked, 0], pairs[whoFucked, 1], whoFucked);
         }
+    }
+
+    public void ProcreateMii(GameObject _mii, GameObject _father, GameObject _mother, int _parentsId)
+    {
+        Sprite domHead = (Random.Range(0, 1) > 0.5 ? _father.GetComponent<Human>().head.sprite : _mother.GetComponent<Human>().head.sprite);
+        Sprite domEyes = (Random.Range(0, 1) > 0.5 ? _father.GetComponent<Human>().eyeL.sprite : _mother.GetComponent<Human>().eyeL.sprite);
+        Sprite domBrows = (Random.Range(0, 1) > 0.5 ? _father.GetComponent<Human>().browL.sprite : _mother.GetComponent<Human>().browL.sprite);
+        Sprite domNose = (Random.Range(0, 1) > 0.5 ? _father.GetComponent<Human>().nose.sprite : _mother.GetComponent<Human>().nose.sprite);
+        Sprite domMouth = (Random.Range(0, 1) > 0.5 ? _father.GetComponent<Human>().mouth.sprite : _mother.GetComponent<Human>().mouth.sprite);
+        Color domSkin = (Random.Range(0, 1) > 0.5 ? _father.GetComponent<Human>().head.color : _mother.GetComponent<Human>().head.color);
+        Color domIris = (Random.Range(0, 1) > 0.5 ? _father.GetComponent<Human>().eyeL.material.GetColor("_IrisColor") : _mother.GetComponent<Human>().eyeL.material.GetColor("_IrisColor"));
+
+        _mii.GetComponent<Human>().SetAllGenes(domHead, domEyes, domBrows, domMouth, domNose, domSkin, domIris);
+        _mii.GetComponent<Human>().SetPairId(0);
+        _mii.GetComponent<Human>().SetParentId(_parentsId);
+
+        Mutate(_mii);
+    }
+
+    public void Mutate(GameObject _mii)
+    {
+        Vector3 mutPos;
+        Vector3 mutScale;
+
+        // Head Mutation
+        mutPos = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount), Random.Range(-bodyMutationAmmount, bodyMutationAmmount), 0);
+        mutScale = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), 0);
+        _mii.GetComponent<Human>().MutateHead(mutPos, mutScale);
+
+        // Eyes Mutation
+        mutPos = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount), Random.Range(-bodyMutationAmmount, bodyMutationAmmount), 0);
+        mutScale = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), 0);
+        _mii.GetComponent<Human>().MutateEyes(mutPos, mutScale);
+
+        // Brows Mutation
+        mutPos = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount), Random.Range(-bodyMutationAmmount, bodyMutationAmmount), 0);
+        mutScale = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), 0);
+        _mii.GetComponent<Human>().MutateBrows(mutPos, mutScale);
+
+        // Mouth Mutation
+        mutPos = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount), Random.Range(-bodyMutationAmmount, bodyMutationAmmount), 0);
+        mutScale = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), 0);
+        _mii.GetComponent<Human>().MutateMouth(mutPos, mutScale);
+
+        // Nose Mutation
+        mutPos = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount), Random.Range(-bodyMutationAmmount, bodyMutationAmmount), 0);
+        mutScale = new Vector3(Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), Random.Range(-bodyMutationAmmount, bodyMutationAmmount).Remap(-bodyMutationAmmount, bodyMutationAmmount, 0.5f, 2f), 0);
+        _mii.GetComponent<Human>().MutateNose(mutPos, mutScale);
+
+        // Skin Mutation
+        int rMul = Random.Range(0, 1) > 0.5 ? -1 : 1;
+        int gMul = Random.Range(0, 1) > 0.5 ? -1 : 1;
+        int bMul = Random.Range(0, 1) > 0.5 ? -1 : 1;
+        _mii.GetComponent<Human>().MutateSkin(colorMutationAmmount * rMul, colorMutationAmmount * gMul, colorMutationAmmount * bMul);
+
+        // Iris Mutation
+        rMul = Random.Range(0, 1) > 0.5 ? -1 : 1;
+        gMul = Random.Range(0, 1) > 0.5 ? -1 : 1;
+        bMul = Random.Range(0, 1) > 0.5 ? -1 : 1;
+        _mii.GetComponent<Human>().MutateIris(colorMutationAmmount * rMul, colorMutationAmmount * gMul, colorMutationAmmount * bMul);
+
     }
 
     public void RandomizeMiis(){
@@ -99,7 +155,6 @@ public class Genetics_Mii : MonoBehaviour
 
             miis[i].GetComponent<Human>().SetAllGenes(randomHead, randomEyes, randomBrows, randomMouth, randomNose, randomSkin, randomIris);
         }
-        
     }
 
     public void SpawnMiis(){
@@ -111,6 +166,7 @@ public class Genetics_Mii : MonoBehaviour
 
     public void Start(){
         SpawnMiis();
+        currentTime = timeDelay;
     }
 
     public void Update(){
@@ -119,5 +175,25 @@ public class Genetics_Mii : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.P))
             PairMiis();
+
+        if (Input.GetKeyDown(KeyCode.N))
+            NewGeneration();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            ToggleTime();
+
+
+        if (advanceTime)
+        {
+            if (currentTime <= 0)
+            {
+                PairMiis();
+                NewGeneration();
+                currentTime = timeDelay;
+            } else
+            {
+                currentTime -= Time.deltaTime;
+            }
+        }
     }
 }
--- AI Bois/Assets/Scripts/Human.cs
+++ AI Bois/Assets/Scripts/Human.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Human : MonoBehaviour
 {
@@ -24,6 +25,9 @@ public class Human : MonoBehaviour
     public Image mouth;
     public Image nose;
 
+    public TextMeshProUGUI pairId;
+    public TextMeshProUGUI parentId;
+
     public void SetAllGenes(Sprite _head, Sprite _eyes, Sprite _brows, Sprite _mouth, Sprite _nose, Color _skin, Color _iris){
         head.sprite = _head;
         head.color = _skin;
@@ -34,7 +38,73 @@ public class Human : MonoBehaviour
         mouth.sprite = _mouth;
         nose.sprite = _nose;
 
-        eyeL.GetComponent<Image>().material.SetColor("_IrisColor", _iris);
-        eyeR.GetComponent<Image>().material.SetColor("_IrisColor", _iris);
+        eyeL.material.SetColor("_IrisColor", _iris);
+        eyeR.material.SetColor("_IrisColor", _iris);
+    }
+
+    public void MutateHead(Vector2 _position, Vector3 _scale)
+    {
+        head.GetComponent<RectTransform>().anchoredPosition += _position;
+        head.GetComponent<RectTransform>().localScale = _scale;
+    }
+
+    public void MutateEyes(Vector2 _position, Vector3 _scale)
+    {
+        eyeL.GetComponent<RectTransform>().anchoredPosition += _position;
+        eyeL.GetComponent<RectTransform>().localScale = _scale;
+
+        Vector2 invPosition = new Vector2(-_position.x, _position.y);
+        Vector3 invScale = new Vector3(-_scale.x, _scale.y, _scale.z);
+
+        eyeR.GetComponent<RectTransform>().anchoredPosition += invPosition;
+        eyeR.GetComponent<RectTransform>().localScale = invScale;
+    }
+
+    public void MutateBrows(Vector2 _position, Vector3 _scale)
+    {
+        browL.GetComponent<RectTransform>().anchoredPosition += _position;
+        browL.GetComponent<RectTransform>().localScale = _scale;
+
+        Vector2 invPosition = new Vector2(-_position.x, _position.y);
+        Vector3 invScale = new Vector3(-_scale.x, _scale.y, _scale.z);
+
+        browR.GetComponent<RectTransform>().anchoredPosition += invPosition;
+        browR.GetComponent<RectTransform>().localScale = invScale;
+    }
+
+    public void MutateMouth(Vector2 _position, Vector3 _scale)
+    {
+        mouth.GetComponent<RectTransform>().anchoredPosition += _position;
+        mouth.GetComponent<RectTransform>().localScale = _scale;
+    }
+
+    public void MutateNose(Vector2 _position, Vector3 _scale)
+    {
+        nose.GetComponent<RectTransform>().anchoredPosition += _position;
+        nose.GetComponent<RectTransform>().localScale = _scale;
+    }
+
+    public void MutateSkin(float _offsetR, float _offsetG, float _offsetB)
+    {
+        head.color = new Color(head.color.r + _offsetR, head.color.g + _offsetG, head.color.b + _offsetB);
+    }
+
+    public void MutateIris(float _offsetR, float _offsetG, float _offsetB)
+    {
+        Color og = eyeL.material.GetColor("_IrisColor");
+        Color mutColor = new Color((og.r + _offsetR), og.g + _offsetG, og.b + _offsetB);
+
+        eyeL.material.SetColor("_IrisColor", mutColor);
+        eyeR.material.SetColor("_IrisColor", mutColor);
+    }
+
+    public void SetPairId(int _id)
+    {
+        pairId.text = _id.ToString();
+    }
+
+    public void SetParentId(int _id)
+    {
+        parentId.text = _id.ToString();
     }
 }
