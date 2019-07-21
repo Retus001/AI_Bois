4e6f7670cf623af48ee19af9f201651b6d76ec53
Retus001 <retus001@gmail.com>
2019-07-21 01:45:11 -0500

    Content Update
    + New CellShaded Shader
    + Art Assets
    Scene Setup

---
+++ AI Bois/Assets/Scripts/CityBois/CB_House.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CB_House : MonoBehaviour
+{
+    public int maxResidents;
+    public List<GameObject> residents = new List<GameObject>();
+    public Transform entrance;
+    public Transform[] inside;
+
+    public void StoreResident(GameObject _resident){
+        if (residents.Count < maxResidents){
+            residents.Add(_resident);
+            _resident.transform.position = inside[residents.Count].position;
+        } else {
+            Debug.Log("Not Enough Space at: " + gameObject.name + " to house: " + _resident);
+        }
+        
+    }
+
+    public void DropResident(GameObject _resident){
+        residents.Remove(_resident);
+        _resident.transform.position = entrance.position;
+    }
+}
--- AI Bois/Assets/Scripts/CityBois/CB_HumanController.cs
+++ AI Bois/Assets/Scripts/CityBois/CB_HumanController.cs
@@ -6,6 +6,9 @@ public class CB_HumanController : MonoBehaviour
 {
     // Internal
     public float speed;
+    public float velocityGainDelay;
+    public float velocityFalloffDelay;
+    private Rigidbody rig;
 
     // External
     public Transform house;
@@ -19,7 +22,7 @@ public class CB_HumanController : MonoBehaviour
         RUNNING
     }
 
-    public BEHAVIOURSTATES currentState;
+    public BEHAVIOURSTATES currentState = BEHAVIOURSTATES.MOVING;
 
     public void SetState(BEHAVIOURSTATES _state)
     {
@@ -33,6 +36,26 @@ public class CB_HumanController : MonoBehaviour
 
     void Update()
     {
+        switch(currentState) {
+            default:
+                Debug.Log("Entered Invalid State");
+            break;
+
+            case BEHAVIOURSTATES.IDLE:
+                rig.velocity = Vector3.Lerp(rig.velocity, Vector3.zero, velocityFalloffDelay);
+            break;
+
+            case BEHAVIOURSTATES.MOVING:
+                rig.velocity = Vector3.Lerp(rig.velocity, transform.forward * speed * Time.deltaTime, velocityGainDelay);
+            break;
+
+            case BEHAVIOURSTATES.RUNNING:
+                rig.velocity = Vector3.Lerp(rig.velocity, transform.forward * speed * 2f * Time.deltaTime, velocityGainDelay);
+            break;
+        }
+    }
+
+    public void FollowTarget(Transform _target){
         
     }
 }
